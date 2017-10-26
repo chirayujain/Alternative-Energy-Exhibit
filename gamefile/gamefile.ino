a05943cd66d97039048860c49e4e0496e709e2d6
Chirayu Jain <jain.chirayu98@gmail.com>
2017-10-26 13:25:04 -0400

    updates
    Test

--- gamefile/gamefile.ino
+++ gamefile/gamefile.ino
@@ -1,7 +1,12 @@
 /*Stacked Cornerstone Exhibit 
  * 
  * Features to implement:
- *  timer
+ *  timer - 30 
+ *  rounds: 2-3
+ *  minecraft based resources
+ *  fleshed out point based system
+ *  
+ *  
  *  index for 18 energy sources
  *  18 buttons
  *  animations for neopixel weather
