c85e41d20b4426ec23080b622e7332eb67ea4574
Chirayu Jain <jain.chirayu98@gmail.com>
2017-10-26 12:54:29 -0400

    Created files
    Added list of features

---
+++ gamefile/gamefile.ino
@@ -0,0 +1,21 @@
+/*Stacked Cornerstone Exhibit 
+ * 
+ * Features to implement:
+ *  timer
+ *  index for 18 energy sources
+ *  18 buttons
+ *  animations for neopixel weather
+ *  minecraft animations for time
+ *  
+ * 
+ * 
+ * 
+ */
+void setup() {
+  
+}
+
+void loop() {
+
+
+}
